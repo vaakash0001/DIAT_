083ceff0b4dbc53a16da37b716c8931d4752d0c8
vaakash0001 <76882797+vaakash0001@users.noreply.github.com>
2024-08-21 17:12:12 +0000

    commit before playfair hello

---
+++ CS603 Applied Cryptography/CS603-AppliedCryptographyLAB/CS603 Applied Cryptography Lab/Playfair-Lab1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+
+// function to remove everything else other than alphabet and digits.
+// void cleanPlainText(char* plainTxt, int txtLen){
+    
+//     for(int i=0; i < txtLen; i++){
+
+//         char chr = plainTxt[i];
+//         bool validChar = ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z' ) ||(c >= '0' && c <= '9'));
+        
+//         if(validChar){
+//             plainTxt[]
+//         }
+//     }
+
+// } 
+
+int main(){
+    std::cout<<"hello!";
+}
